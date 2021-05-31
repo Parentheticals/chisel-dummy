e83a7dcb6df73767ee7b566791efd5096a2f441c
David Wang <davidwwangwang@gmail.com>
2021-05-31 15:45:06 +0200

    badcounter name fix

--- src/main/scala/counter/BadCounter.scala
+++ src/main/scala/counter/BadCounter.scala
@@ -3,7 +3,7 @@ package counter
 import chisel3._
 import chisel3.util._
 
-class Counter(width: Int) extends Module {
+class BadCounter(width: Int) extends Module {
   val io = IO(new Bundle {
     val in  = Input(UInt(width.W))
     val out = Output(UInt(width.W))
