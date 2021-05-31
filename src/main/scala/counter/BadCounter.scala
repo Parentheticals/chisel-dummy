98dd3d46e6527658bbc7dd319253f5533c119d12
David Wang <davidwwangwang@gmail.com>
2021-05-31 16:37:01 +0200

    Modified bad Counter

--- src/main/scala/counter/BadCounter.scala
+++ src/main/scala/counter/BadCounter.scala
@@ -8,9 +8,9 @@ class BadCounter(width: Int) extends Module {
     val out = Output(UInt(width.W))
   })
   
-  val reg1 = RegInit(1.U(width.W))
+  val reg1 = Reg(UInt(width.W))
   val reg2 = RegInit(0.U(width.W))
-  reg1 := reg1 + 0.U
+  reg1 := 1.U
   reg2 := reg2 + reg1
   io.out := reg2
 }
