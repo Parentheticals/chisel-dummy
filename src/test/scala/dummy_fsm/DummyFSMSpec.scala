d4a5a7ac609a341178d85c9b6560aa173818c35f
David Wang <davidwwangwang@gmail.com>
2021-06-01 13:36:39 +0200

    I'm just modifing single letters

--- src/test/scala/dummy_fsm/DummyFSMSpec.scala
+++ src/test/scala/dummy_fsm/DummyFSMSpec.scala
@@ -7,7 +7,7 @@ import chisel3.experimental.BundleLiterals._
 
 class DummyFSMSpec extends FreeSpec with ChiselScalatestTester {
   "Dummy FSM test" in {
-    test(new Dummy_FSM(4, 6)) { dut =>
+    test(new Dummy_FSM(4, 6.U)) { dut =>
         dut.io.out.ready.poke(false.B)
         dut.io.in.valid.poke(false.B)
         dut.clock.step(3)
